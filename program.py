1614fad0188df20c06cbea0c8f2c54cf32a81627
Al Fathi Ayyash <150584442+Alfaashh@users.noreply.github.com>
2024-09-23 23:04:21 +0700

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,8 @@ def coin_change_greedy(coins, amount):
   coin_count = 0
   coin_used = []
   
+  coins.sort(reverse=True)
+  
   for coin in coins:
     while amount >= coin:
       amount -= coin
@@ -13,12 +15,13 @@ def coin_change_greedy(coins, amount):
   else:
     return -1, []  #Error jika coin tidak yang tersedia tidak bisa untuk menukar nilai uang
 
-coins = [10, 5, 2, 1]
+coins = [1, 2, 5, 10]
+print(f"Koin yang tersedia: {coins}")
 amount = int(input("Nilai uang yang ingin ditukarkan: "))
 
 count, used = coin_change_greedy(coins, amount)
 if count != -1:
   print(f"Koin minimum yang dibutuhkan: {count}")
-  print(f"Koin yang dipakai: {used}")
+  print(f"Koin yang terpakai: {used}")
 else:
   print("Tidak bisa menukar nilai uang dengan nominal koin yang ada.")
